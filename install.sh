2171d3f1effb500b89ffa11335596103e24ea171
T2HASH <92183808+T2HASH@users.noreply.github.com>
2026-06-20 02:58:59 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@ set -euo pipefail
 
 VERSION="2.3"
 GO_VERSION="1.22.4"
-REPO_URL="https://github.com/T2HASH/DPISCANING-T2HASH
-"
-RAW_URL="https://raw.githubusercontent.com/T2HASH/DPISCANING-T2HASH
-/main"
+REPO_URL="https://github.com/T2HASH/DPISCANING-T2HASH"
+RAW_URL="https://raw.githubusercontent.com/T2HASH/DPISCANING-T2HASH/main"
 INSTALL_DIR="/usr/local"
 BINARY_NAME="t2hash-scanner"
 SERVICE_NAME="t2hash-scanner"
